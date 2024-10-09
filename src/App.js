e273236268286f93d3388ec1a2de865a35335b6f
irekmiernik <129183189+irekmiernik@users.noreply.github.com>
2024-10-09 16:16:48 +0200

    initial comitment with updating icons, public index.html, App.js based on currency-conventer application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="header">Kalkulator walutowy</h1>
+      <div className="container">
+        <form className="form">
+          <p>
+            <label className="form__label"> Podaj kwotę wybranej waluty:
+              <input className="js-inputAmount" type="number" step="0.01" min="0.01" required
+                placeholder="Twoja wartość" autoFocus />
+              <select className="js-inputCurrency"></select>
+              <strong className="form__label form__labelEqual"> = </strong>
+              <input className="js-outputAmount" type="number" readOnly placeholder="Obliczona wartość" />
+              <select className="js-outputCurrency"></select>
+            </label>
+          </p>
+          <p>
+            <strong className="form__label form__labelRates js-labelUpdateExchangeRates">
+              Pokaż kursy walut
+            </strong>
+            <strong className="form__label form__labelRates js-labelInputExchangeRates" hidden>
+              Ukryj kursy walut:
+            </strong>
+            <input className="js-inputExchangeRate" type="number" step="0.0001" min="0.0001" required hidden
+              placeholder="Podaj kurs" />
+            <select className="js-inputExchangeRateCurrency" hidden></select>
+          </p>
+        </form>
+      </div>
+      <footer className="footer">Wszelkie prawa zastrzeżone Ireneusz Miernik 2023</footer>
+    </>
   );
 }
-
-export default App;
