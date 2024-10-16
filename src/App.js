cff6ceca10b5dff0b55979a660809a5f8a6d6d11
irekmiernik <129183189+irekmiernik@users.noreply.github.com>
2024-10-16 14:06:59 +0200

    Interaction for updating the exchange rates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Container from "./Container";
 import Form from "./Form";
+import Curriencies from "./Curriencies";
+import Label from "./Label";
+import Rates from "./Rates";
 
 export default function App() {
+
+  const tableRates = [
+    { id: 0, curriency: "PLN", rate: 1.00 },
+    { id: 1, curriency: "EUR", rate: 4.56 },
+    { id: 2, curriency: "USD", rate: 3.95 },
+    { id: 3, curriency: "GBP", rate: 5.30 },
+    { id: 4, curriency: "CHF", rate: 4.88 },
+  ];
+
+  const [showRates, setShowRates] = useState(false);
+
+  const toggleShowRates = () => setShowRates(showRates => !showRates);
+
   return (
     <>
       <Header title="Kalkulator walutowy" />
-      <Container body={<Form />} />
+      {showRates
+        ? <Container body={<Form curriencies={<Curriencies tableRates={tableRates} />}
+          body={<Rates curriencies={< Curriencies tableRates={tableRates} />} toggleShowRates={toggleShowRates} />} />} />
+
+        : <Container body={<Form curriencies={<Curriencies tableRates={tableRates} />}
+          body={<Label toggleShowRates={toggleShowRates} />} />} />
+      }
       <Footer date="2024" />
     </>
   );
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,29 +1,20 @@
 import "./style.css";
 
-const Form = () => (
-  <form className="form">
-    <p>
-      <label className="form__label"> Podaj kwotę wybranej waluty:
+const Form = ({ curriencies, body }) => (
+  <>
+    <form className="form">
+      <p className="form__label">
+        Podaj kwotę wybranej waluty:
         <input type="number" step="0.01" min="0.01" required
           placeholder="Twoja wartość" autoFocus />
-        <select ></select>
+        {curriencies}
         <strong className="form__label form__labelEqual"> = </strong>
         <input type="number" readOnly placeholder="Obliczona wartość" />
-        <select ></select>
-      </label>
-    </p>
-    <p>
-      <strong className="form__label form__labelRates">
-        Pokaż kursy walut
-      </strong>
-      <strong className="form__label form__labelRates" hidden>
-        Ukryj kursy walut:
-      </strong>
-      <input type="number" step="0.0001" min="0.0001" required hidden
-        placeholder="Podaj kurs" />
-      <select hidden></select>
-    </p>
-  </form>
+        {curriencies}
+      </p>
+      {body}
+    </form>
+  </>
 );
 
 export default Form;
